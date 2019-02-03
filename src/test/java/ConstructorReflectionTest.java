e95ead1a62f75ead36361ca4f79698e4cd515b52
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-03 11:15:34 +0100

    test scaffold

---
+++ src/test/java/ConstructorReflectionTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+/**
+ * Created by mtumilowicz on 2019-02-03.
+ */
+public class ConstructorReflectionTest {
+    @Test
+    public void getConstructors() {
+        
+    }
+    
+    @Test
+    public void getDeclaredConstructors() {
+        
+    }
+    
+    @Test(expected = NoSuchMethodException.class)
+    public void getConstructor_notFound() {
+        
+    }
+    
+    @Test(expected = NoSuchMethodException.class)
+    public void getConstructor_private() {
+        
+    }
+    
+    @Test
+    public void getConstructor_public() {
+        
+    }
+}
