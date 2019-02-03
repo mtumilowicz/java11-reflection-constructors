c2d2532984bcde6bea1fde0e9592d079e68edad5
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-03 11:15:20 +0100

    class structure

---
+++ src/main/java/Child.java
@@ -0,0 +1,27 @@
+/**
+ * Created by mtumilowicz on 2019-02-03.
+ */
+class Child extends Parent {
+    private Child() {
+
+    }
+
+    Child(String name) {
+
+    }
+
+    protected Child(int count) {
+
+    }
+
+    public Child(String name, int count) {
+
+    }
+}
+
+
+class Parent {
+    public Parent() {
+
+    }
+}
