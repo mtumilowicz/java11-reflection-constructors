71909f1ee96b776e1ff73c6e3a3371684b8a7b6f
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-03 11:24:14 +0100

    getConstructor / getDeclaredConstructor

--- src/test/java/ConstructorReflectionTest.java
+++ src/test/java/ConstructorReflectionTest.java
@@ -10,17 +10,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by mtumilowicz on 2019-02-03.
  */
 public class ConstructorReflectionTest {
-    
+
     @Test
     public void getConstructors() {
         var constructors = Child.class.getConstructors();
-        
+
         assertThat(constructors.length, is(1));
-        
+
         var constructorsAsString = Arrays.toString(constructors);
         assertThat(constructorsAsString, containsString("public Child(java.lang.String,int)"));
     }
-    
+
     @Test
     public void getDeclaredConstructors() {
         var constructors = Child.class.getDeclaredConstructors();
@@ -28,39 +28,42 @@ public class ConstructorReflectionTest {
         assertThat(constructors.length, is(4));
 
         var constructorsAsString = Arrays.toString(constructors);
-        assertThat(constructorsAsString, containsString("public Child(java.lang.String,int)")); 
-        assertThat(constructorsAsString, containsString("protected Child(int)")); 
-        assertThat(constructorsAsString, containsString("Child(java.lang.String)")); 
-        assertThat(constructorsAsString, containsString("private Child()")); 
-    }
-    
-    @Test(expected = NoSuchMethodException.class)
-    public void getConstructor_notFound() {
-        
-    }
-    
-    @Test(expected = NoSuchMethodException.class)
-    public void getConstructor_private() {
-        
-    }
-    
-    @Test
-    public void getConstructor_public() {
-        
+        assertThat(constructorsAsString, containsString("public Child(java.lang.String,int)"));
+        assertThat(constructorsAsString, containsString("protected Child(int)"));
+        assertThat(constructorsAsString, containsString("Child(java.lang.String)"));
+        assertThat(constructorsAsString, containsString("private Child()"));
     }
 
     @Test(expected = NoSuchMethodException.class)
-    public void getDeclaredConstructor_notFound() {
+    public void getConstructor_notFound() throws NoSuchMethodException {
+        Child.class.getConstructor(int.class, int.class);
+    }
 
+    @Test(expected = NoSuchMethodException.class)
+    public void getConstructor_private() throws NoSuchMethodException {
+        Child.class.getConstructor();
     }
 
     @Test
-    public void getDeclaredConstructor_private() {
+    public void getConstructor_public() throws NoSuchMethodException {
+        assertThat(Child.class.getConstructor(String.class, int.class).toGenericString(),
+                is("public Child(java.lang.String,int)"));
+    }
 
+    @Test(expected = NoSuchMethodException.class)
+    public void getDeclaredConstructor_notFound() throws NoSuchMethodException {
+        Child.class.getDeclaredConstructor(int.class, int.class);
     }
 
     @Test
-    public void getDeclaredConstructor_public() {
+    public void getDeclaredConstructor_private() throws NoSuchMethodException {
+        assertThat(Child.class.getDeclaredConstructor().toGenericString(),
+                is("private Child()"));
+    }
 
+    @Test
+    public void getDeclaredConstructor_public() throws NoSuchMethodException {
+        assertThat(Child.class.getDeclaredConstructor(String.class, int.class).toGenericString(),
+                is("public Child(java.lang.String,int)"));
     }
 }
